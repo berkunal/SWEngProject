99a69f8ba2a1813652565956520421c1aa9cd34e
Berk Unal <cesmeli.berk@gmail.com>
2017-03-17 18:34:16 +0300

    Visually arranged the login screen

---
+++ Geotrix/src/gamepackage/Game.java
@@ -0,0 +1,14 @@
+package gamepackage;
+
+import javax.swing.JPanel;
+
+class Game extends JPanel{
+
+	private static final long serialVersionUID = 1L;
+
+	public Game() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Hello?");
+	}
+
+}
--- Geotrix/src/gamepackage/Menu.java
+++ Geotrix/src/gamepackage/Menu.java
@@ -1,59 +1,86 @@
 package gamepackage;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-public class Menu extends JFrame implements ActionListener{
+class Menu extends JFrame{
 
 	private static final long serialVersionUID = 1L;
-	private JMenuItem item1 = new JMenuItem("Credits");
-	private JMenuItem item2 = new JMenuItem("Exit");
+	private int frameBoundX = 960;//frame x axis
+	private int frameBoundY = 540;//frame y axis
 
 	public Menu() {
 		// TODO Auto-generated constructor stub
 		JFrame f = new JFrame("Geotrix");
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JMenuBar menubar = new JMenuBar();
-		JMenu menu1 = new JMenu("File");
-		JMenu menu2 = new JMenu("Help");
+		//Login Panel
+		JPanel loginPanel = new JPanel();
+		loginPanel.setLayout(null);
+		loginPanel.setBounds(0, 0, frameBoundX, frameBoundY);
 		
-		item1.addActionListener(this);
-		item2.addActionListener(this);
+		//Logo Label
+		JLabel logoLabel = new JLabel();
+		logoLabel.setLayout(null);
+		int i = frameBoundY - 520;
+		logoLabel.setBounds(frameBoundX/2 - 200, i/2, 400, 190);
+		logoLabel.setBackground(Color.black);
+		logoLabel.setOpaque(true);
 		
-		menu2.add(item1);
-		menu1.add(item2);
+		//username
+		JTextField usernameTextField = new JTextField("Username", 15);
+		usernameTextField.setBounds(frameBoundX/2 - 200, i/2 + 200, 400, 100);
 		
-		menubar.add(menu1);
-		menubar.add(menu2);
+		//password
+		JTextField passwordTextField = new JPasswordField("Passwd", 15);
+		passwordTextField.setBounds(frameBoundX/2 - 200, i/2 + 310, 400, 100);
 		
-		f.setJMenuBar(menubar);
+		//sign in
+		JButton signInButton = new JButton("Sign In");
+		signInButton.setBounds(frameBoundX/2 - 200, i/2 + 420, 195, 100);
 		
-		f.setSize(360, 480);
+		//log in
+		JButton logInButton = new JButton("Log In");
+		logInButton.setBounds(frameBoundX/2 + 5, i/2 + 420, 195, 100);
+		//when login button clicked
+		logInButton.addActionListener( new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//remove login panel
+				loginPanel.setVisible(false);
+				f.remove(loginPanel);
+				//call game panel
+				JPanel gamePanel = new Game();
+				f.add(gamePanel);
+				
+			}
+		});
+
+		//adding labels and textfields to login panel
+		loginPanel.add(logoLabel);
+		loginPanel.add(usernameTextField);
+		loginPanel.add(passwordTextField);
+		loginPanel.add(signInButton);
+		loginPanel.add(logInButton);
+		//adding login panle to main frame
+		f.add(loginPanel);
+		
+		f.setSize(frameBoundX, frameBoundY);
 		f.setResizable(false);
 		f.setVisible(true);
+		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Menu();
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if ( e.getSource() == item1 ) {
-			JOptionPane.showMessageDialog(null, "Berk Ünal\n20140702009");
-		}
-		if ( e.getSource() == item2 ) {
-			System.exit(0);
-		}
 	}
 
 }
